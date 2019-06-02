aad45805a7e892b1fc44bc4185250d4a92ea859a
Alessio Morale <alessiomorale@gmail.com>
2019-06-03 00:22:34 +0100

    Fix configurations mapping

--- unav2_hardware/scripts/configuration_manager.py
+++ unav2_hardware/scripts/configuration_manager.py
@@ -29,31 +29,32 @@ from unav2_hardware.msg import SafetyConfig
 # msg: message instance
 # propertynames: list of property to copy from config to message or tuples when source/dest name differs
 configurationSets = [
-    {"namespace": "bridge", "config": UnavBridgeConfig, "msg": BridgeConfig(), 
+    {"namespace": "bridge", "config": UnavBridgeConfig, "msg": BridgeConfig(),
         "propertynames": ["pwm_dead_time", "pwm_frequency", "bridge_enable_polarity", "bridge_disable_mode_outa", "bridge_disable_mode_outb", "current_offset", "current_gain", "volt_gain", "volt_offset"]},
-    {"namespace": "encoder", "config": UnavEncoderConfig, "msg": EncoderConfig(), 
+    {"namespace": "encoder", "config": UnavEncoderConfig, "msg": EncoderConfig(),
         "propertynames": ["cpr", "position", "has_z_index", "channels"]},
-    {"namespace": "limits", "config": UnavLimitsConfig, "msg": LimitsConfig(),    
-        "propertynames": ['position','velocity','current','effort','pwm']},
+    {"namespace": "limits", "config": UnavLimitsConfig, "msg": LimitsConfig(),
+        "propertynames": ['position', 'velocity', 'current', 'effort', 'pwm']},
     {"namespace": "mechanical", "config": UnavMechanicalConfig, "msg": MechanicalConfig(),
-        "propertynames": ['ratio','rotation0','rotation1','rotation2','rotation3']},
+        "propertynames": ['ratio', 'rotation0', 'rotation1', 'rotation2', 'rotation3']},
     {"namespace": "operation", "config": UnavOperationConfig, "msg": OperationConfig(),
-        "propertynames": ['settings_command','operation_mode']},
+        "propertynames": ['settings_command', 'operation_mode', 'pid_debug']},
     {"namespace": "pid", "config": UnavPIDConfig, "msg": PIDConfig(),
-        "propertynames": ["vel_kp","vel_ki","vel_kd","vel_kaw","cur_kp","cur_ki","cur_kd","cur_kaw","vel_frequency","cur_frequency","cur_enable"]},
+        "propertynames": ["vel_kp", "vel_ki", "vel_kd", "vel_kaw", "cur_kp", "cur_ki", "cur_kd", "cur_kaw", "vel_frequency", "cur_frequency", "cur_enable"]},
     {"namespace": "safety", "config": UnavSafetyConfig, "msg": SafetyConfig(),
-        "propertynames": ["temp_warning","temp_critical","temp_timeout","temp_autorestore","cur_warning","cur_critical","cur_timeout","cur_autorestore","slope_time","bridge_off","timeout"]},
+        "propertynames": ["temp_warning", "temp_critical", "temp_timeout", "temp_autorestore", "cur_warning", "cur_critical", "cur_timeout", "cur_autorestore", "slope_time", "bridge_off", "timeout"]},
 ]
 
-MsgTTL = 30 # max retries for a message
+MsgTTL = 30  # max retries for a message
 rootNamespace = "unav2"
 PublishersNamespace = rootNamespace + "/config"
 ackNamespace = rootNamespace + "/status/ack"
 messageQueue = []
-ackQueue = Queue(maxsize=40) 
+ackQueue = Queue(maxsize=40)
 srv = []
 messageQueueLock = Lock()
 publishers = {}
+
 
 def copyProperties(source, dest, propertynames):
     for p in propertynames:
@@ -66,15 +67,17 @@ def copyProperties(source, dest, propertynames):
         v = getattr(source, pin)
         setattr(dest, pout, v)
 
+
 def publishConfig(cfgtype, config):
     for msgcfg in [msg for msg in configurationSets if msg["config"] is cfgtype]:
         msg = {"msg": msgcfg["msg"], "ttl": MsgTTL}
         copyProperties(config, msg["msg"], msgcfg["propertynames"])
         msg["msg"].transactionId = randint(0, 2**32 - 1)
-        #msgcfg["publisher"].publish(msg)
-        
+        # msgcfg["publisher"].publish(msg)
+
         messageQueueLock.acquire()
-        [messageQueue.remove(m) for m in messageQueue if m["msg"] == msg["msg"]]
+        [messageQueue.remove(m)
+         for m in messageQueue if m["msg"] == msg["msg"]]
         messageQueue.append(msg)
         messageQueueLock.release()
 
@@ -82,6 +85,7 @@ def publishConfig(cfgtype, config):
         return
     rospy.logwarn(
         "Configuration not found for Dynamic Config message " + str(cfgtype))
+
 
 def bindCallback(msgtype):
     def func1(*args):
@@ -92,52 +96,62 @@ def bindCallback(msgtype):
     func1.__name__ = "callbackConfig" + msgtype.__name__
     return func1
 
+
 def configAckCallback(data):
     rospy.logdebug("Received ack" + str(data))
-    ackQueue.put(data.data)    
+    ackQueue.put(data.data)
+
 
 def run_node():
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         try:
-            while True: 
+            while True:
                 ack = ackQueue.get(False, 0)
-                msgs = [msg for msg in messageQueue if msg["msg"].transactionId == ack]
+                msgs = [
+                    msg for msg in messageQueue if msg["msg"].transactionId == ack]
                 if len(msgs) == 0:
-                    rospy.logwarn("Received Ack for a non existing transaction: " + str(ack))
+                    rospy.logwarn(
+                        "Received Ack for a non existing transaction: " + str(ack))
                 else:
                     for msg in msgs:
                         messageQueue.remove(msg)
-                        rospy.logdebug("Transaction completed for message " + str(msg))
+                        rospy.logdebug(
+                            "Transaction completed for message " + str(msg))
         except Empty:
             pass
         messageQueueLock.acquire()
-        for msg in messageQueue :
+        for msg in messageQueue:
             for cfg in [cfg for cfg in configurationSets if cfg["msg"] == msg["msg"]]:
-                rospy.logdebug("Sending message " + str(msg["msg"]) + ", ttl:" + str(msg["ttl"]))
-                publishers[cfg["config"].__name__].publish(msg["msg"]) 
+                rospy.logdebug("Sending message " +
+                               str(msg["msg"]) + ", ttl:" + str(msg["ttl"]))
+                publishers[cfg["config"].__name__].publish(msg["msg"])
             msg["ttl"] = msg["ttl"] - 1
             if msg["ttl"] <= 0:
-                rospy.logerr("No acknowledge received for transaction " + str(msg["msg"].transactionId) + ", message discarded")
+                rospy.logerr("No acknowledge received for transaction " +
+                             str(msg["msg"].transactionId) + ", message discarded")
                 messageQueue.remove(msg)
         messageQueueLock.release()
 
         rate.sleep()
 
+
 if __name__ == "__main__":
     try:
         rospy.init_node("unav2_configuration", anonymous=False)
-        MsgTTL = int(rospy.get_param('~msgttl','10'))
-        
+        MsgTTL = int(rospy.get_param('~msgttl', '10'))
+
         for config in configurationSets:
             # Setup publishers
-            publishers[config["config"].__name__] = rospy.Publisher(PublishersNamespace + "/" + config["namespace"], type(config["msg"]), queue_size=10)
+            publishers[config["config"].__name__] = rospy.Publisher(
+                PublishersNamespace + "/" + config["namespace"], type(config["msg"]), queue_size=10)
 
             # Setup dynamic recofigurre callbacks
-            srv.append(Server(config["config"], bindCallback(config["config"]), config["namespace"]))
-        
+            srv.append(Server(config["config"], bindCallback(
+                config["config"]), config["namespace"]))
+
         rospy.Subscriber(ackNamespace, UInt32, configAckCallback)
-        
+
         run_node()
 
     except rospy.ROSInterruptException:
